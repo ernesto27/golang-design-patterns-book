d652ebdbee721686444c35f5b61fd345b8613e97
ernesto <ernestoponce27@gmail.com>
2023-01-28 17:33:04 -0300

    add folders

---
+++ facade/facade_test.go
@@ -0,0 +1,19 @@
+package facade
+
+import (
+	"testing"
+)
+
+func TestOpenWeatherResponseParse(t *testing.T) {
+	// r := getMockData()
+	// openWeatherMap := CurrentWeatherData{APIKey: ""}
+
+	// weather, err := openWeatherMap.responseParser(r)
+	// if err != nil {
+	// 	t.Fatal("ERR ", err)
+	// }
+
+	// if weather.ID != 3117735 {
+	// 	t.Errorf("Madrid id is 3117735, not %d\n", weather.ID)
+	// }
+}
